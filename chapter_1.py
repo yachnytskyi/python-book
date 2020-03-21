bb57183977b22109ce4a74decde2f0430fbc6cf5
yachnytskyi <34445544+yachnytskyi@users.noreply.github.com>
2020-03-21 21:45:00 +0200

    Initial commit

---
+++ chapter_1.py
@@ -0,0 +1,36 @@
+print("Languages: C#\nJava\nRuby\n\tPython\n\t\tPhP\n\t\t\tJavaScript")
+
+language = "C# "
+print(len(language))
+language = language.rstrip()
+print(len(language))
+
+print("it's apostrophe, hey!")
+
+print("Hey man, what's up?")
+name = "man"
+print(name.title())
+print(name.lower())
+print(name.upper())
+
+print('Hey "the double quotes"! I Can use you here!')
+
+buddha = "Buddha"
+print('He (' + buddha + ') said, "we live in the world of illusions"')
+
+# Move strings and tabs
+person = "con\tfuc\n\ti\n\n\t\tu\t\t\ts"
+print(person)
+print(person.lstrip())
+print(person.rstrip())
+print(person.strip())
+
+print(1 * 8)
+print(4 + 4)
+print(16 / 2)
+print(10 - 2)
+print(2 ** 3)
+
+# My favorite number
+number = 13
+print("My favorite number is " + str(number) + ", that's amazing man!")
