658ea5da20980269a497d72c8b61f2f00db71799
yachnytskyi <yachnytskyi@gmail.com>
2020-04-05 14:42:02 +0300

    Added the aditional code in the Chapter 9 and needing text/json files for the Chapter 9. Finished

--- Eric_Matthes/chapter_9.py
+++ Eric_Matthes/chapter_9.py
@@ -1,4 +1,6 @@
-filename = 'first_text_file.txt'
+import json
+
+#filename = 'first_text_file.txt'
 
 # with open(filename) as fileObject:
 #     contents = fileObject.read()
@@ -113,3 +115,112 @@ filename = 'first_text_file.txt'
 #         print(reasons)
 #         with open(filename, 'a') as fileObject:
 #             fileObject.write(f"{name} likes programming because: {reasons}\n")
+
+
+# while True:
+#     firstNumber = input("Enter the first number")
+#     if firstNumber == 'q':
+#         break
+#     secondNumber = input("Enter the second number")
+#     if secondNumber == 'q':
+#         break
+#     try:
+#         result = int(firstNumber) * int(secondNumber)
+#     except ValueError:
+#         print("Please enter only numbers!")
+#     else:
+#         print(result)
+
+# filename = "cats.txt"
+# catString = ''
+# try:
+#     with open(filename) as fileObject:
+#         lines = fileObject.readlines()
+# except FileNotFoundError:
+#     # print(f"Sorry, but file {filename} does not exist")
+#     pass
+# else:
+#     for line in lines:
+#         catString += line
+#
+# print(catString)
+# print(len(catString))
+#
+# filename = "dogs.txt"
+# dogString = ''
+# try:
+#     with open(filename) as fileObject:
+#         lines = fileObject.readlines()
+# except FileNotFoundError:
+#     print(f"Sorry, but file {filename} does not exist")
+# else:
+#     for line in lines:
+#         dogString += line
+#
+# print(dogString)
+# print(len(dogString))
+
+# filename = 'gutenberg.txt'
+# textString = ""
+# try:
+#     with open(filename) as fileObject:
+#         lines = fileObject.readlines()
+# except FileNotFoundError:
+#     pass
+# else:
+#     for line in lines:
+#         textString += line
+#
+# theCount = textString.count("the")
+# theLowerCount = textString.lower().count("the")
+# print(f"We have {theCount} times the word 'the' in our text")
+# print(f"We have {theLowerCount} times the word 'the'(with underline) in our text")
+
+
+# filename = "favorite_number.json"
+#
+# try:
+#     with open(filename) as fileObject:
+#         favoriteNumber = json.load(fileObject)
+# except FileNotFoundError:
+#     number = input("Enter your number")
+#     with open(filename, 'w') as fileObject:
+#         json.dump(number, fileObject)
+#         print(f"We have written your favorite number {number}!")
+# else:
+#     print(f"Your favorite number is: {favoriteNumber}")
+
+def get_stored_username():
+    filename = 'username.json'
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+
+def get_new_username():
+    username = input("What is your name? ")
+    filename = 'username.json'
+    with open(filename, 'w') as f_obj:
+        json.dump(username, f_obj)
+    return username
+
+
+def greet_user():
+    username = get_stored_username()
+    correctUsername = input(f"Is {username} your correct username? (yes/no)")
+    if correctUsername == 'yes':
+        print("Welcome back, " + username + "!")
+    else:
+        username = get_new_username()
+        print("We'll remember you when you come back, " + username + "!")
+
+
+greet_user()
+
+
+
+
