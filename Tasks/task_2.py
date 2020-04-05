787eb4535a39103a00ae346444b0d0ebf7004122
yachnytskyi <yachnytskyi@gmail.com>
2020-04-05 18:16:14 +0300

    Fixed the code, it was mistake with data

--- Tasks/task_2.py
+++ Tasks/task_2.py
@@ -1,15 +1,19 @@
 import math
 
-sites = iter({
+sites = {
     'Moscow': (550, 370),
     'London': (510, 510),
     'Paris': (480, 480),
-})
+}
+print(sites['Moscow'][0])
 
 distances = {}
-distances['Moscow-London'] = (math.sqrt((550 - 510) ** 2 + (370 - 510) ** 2))
-distances['Moscow-Paris'] = (math.sqrt((550 - 480) ** 2 + (370 - 480) ** 2))
-distances['London-Paris'] = (math.sqrt((510 - 480) ** 2 + (510 - 480) ** 2))
+distances['Moscow-London'] = (math.sqrt((sites['Moscow'][0] - sites['London'][0])
+                                        ** 2 + (sites['Moscow'][1] - sites['London'][1]) ** 2))
+distances['Moscow-Paris'] = (math.sqrt((sites['Moscow'][0] - sites['Paris'][0])
+                                       ** 2 + (sites['Moscow'][1] - sites['Paris'][1]) ** 2))
+distances['London-Paris'] = (math.sqrt((sites['London'][0] - sites['Paris'][0])
+                                       ** 2 + (sites['London'][1] - sites['Paris'][1]) ** 2))
 print(distances)
 
 radius = 42
@@ -18,7 +22,7 @@ print(round(area))
 
 # Далее, пусть есть координаты точки
 point_1 = (23, 34)
-print(math.sqrt(23 ** 2 + 34 ** 2) <= area)
+print(math.sqrt(point_1[0] ** 2 + point_1[1] ** 2) <= area)
 
 point_2 = (30, 30)
-print(math.sqrt(30 ** 2 + 30 ** 2) <= area)
+print(math.sqrt(point_2[0] ** 2 + point_2[1] ** 2) <= area)
